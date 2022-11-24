e2fab218c7dcad579b61b7895dfa57bf2017744e
Amir Ansari <amir143in@gmail.com>
2022-11-24 09:16:19 -0500

    adding project path2

--- src/test/java/com/nopcommerce/testCases/TC_SearchCustomerWithInvalidEmailDDT_007.java
+++ src/test/java/com/nopcommerce/testCases/TC_SearchCustomerWithInvalidEmailDDT_007.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
